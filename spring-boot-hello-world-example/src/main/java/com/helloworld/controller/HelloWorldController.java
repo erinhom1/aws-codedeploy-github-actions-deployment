95b0ff0df62dc431529a211f4326ae800d18945b
erinhom1 <0320129929@ut-tijuana.edu.mx>
2022-09-28 11:33:23 -0700

    test

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. TEST TEST </h1>";
+return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. You made it </h1>";
 }
 }
